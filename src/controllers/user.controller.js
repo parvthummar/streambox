a8bb62f3c49b87517c95ee938125d272cabd4040
parvthummar <parvthummar8@gmail.com>
2025-08-19 14:04:48 +0530

    second1

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -158,7 +158,7 @@ const loginUser = asyncHandler(async (req,res)=>{
 
 const logoutUser = asyncHandler(async (req,res)=>{
     await User.findByIdAndUpdate(
-        req.user._id,
+        req.user._id , 
         {
             $set : {
                 referenceTokens : undefined,
